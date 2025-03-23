c60dd645bca6db3d9939f8627b7af1ddc4a1d6d9
Vincent Jacquelin <vjacquelinpro@gmail.com>
2025-03-23 14:09:58 +0100

    backend fully fonctional

--- controllers/charactersCtrl.js
+++ controllers/charactersCtrl.js
@@ -1,72 +1,75 @@
-require('dotenv').config()
-const axios = require ("axios")
-
+require("dotenv").config();
+const axios = require("axios");
 
 const getAllCharacters = async (req, res) => {
-   
-//prepare the query parameters for API request
-const page = req.query.page;
-const name = req.query.name || null;
+  //prepare the query parameters for API request
+  const page = req.query.page;
+  const name = req.query.name || null;
 
-//pagination
-const limit = 100;
-const skip = (Number(page) - 1) * 100
+  //pagination
+  const limit = 100;
+  const skip = (Number(page) - 1) * 100;
 
-    try {
+  try {
+    //fetch characters' datas
+    const fetchAllCharacters = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${
+        process.env.API_KEY
+      }${skip ? "&skip=" + skip : ""}${limit ? "&limit=" + limit : ""}${
+        name ? "&name=" + name : ""
+      }`
+    );
 
-        //fetch characters' datas
-        const fetchAllCharacters = await axios.get(`https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.API_KEY}${skip ? "&skip=" + skip : ""}${limit ? "&limit=" + limit : ""}${name ? "&name=" + name : ""}`)
-
-        const charactersDatas = fetchAllCharacters.data        
-
-           res.status(200).json(charactersDatas)
-    } catch (error) {
-        res.status(error.status || 500).json(error.message || "Internal server error")
+    if (Object.keys(fetchAllCharacters.data).includes("error")) {
+      throw { status: 500, message: "Internal server error, please refresh" };
     }
-    
-}
+    const charactersDatas = fetchAllCharacters.data;
 
-
+    res.status(200).json(charactersDatas);
+  } catch (error) {
+    res
+      .status(error.status || 500)
+      .json(error.message || "Internal server error");
+  }
+};
 
 const getOneCharacter = async (req, res) => {
-
-    try {
-
-        if(!req.params.id){
-            throw({status: 400, message: "characterId manquant"})
-        }
+  try {
+    if (!req.params.id) {
+      throw { status: 400, message: "characterId manquant" };
+    }
 
     const characterId = req.params.id;
 
-    const fetchOneCharacter = await axios.get(`https://lereacteur-marvel-api.herokuapp.com/character/${characterId}?apiKey=${process.env.API_KEY}`)
-    
-    const characterDatas = fetchOneCharacter.data   
-    
+    const fetchOneCharacter = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/character/${characterId}?apiKey=${process.env.API_KEY}`
+    );
+
+    const characterDatas = fetchOneCharacter.data;
 
     //Fetch character's comic's details and add it to the response
-    const comicsDetailsPromises = []
+    const comicsDetailsPromises = [];
 
     characterDatas.comics.forEach((comicsId) => {
-        const fetchComicsDetails = axios.get(`https://lereacteur-marvel-api.herokuapp.com/comic/${comicsId}?apiKey=${process.env.API_KEY}`)
+      const fetchComicsDetails = axios.get(
+        `https://lereacteur-marvel-api.herokuapp.com/comic/${comicsId}?apiKey=${process.env.API_KEY}`
+      );
 
-        comicsDetailsPromises.push(fetchComicsDetails)
-        
-    })
-    
-    const comicsDetails = await  Promise.all(comicsDetailsPromises)
+      comicsDetailsPromises.push(fetchComicsDetails);
+    });
 
-    const comicsDetailsDatas = comicsDetails.map((response) => response.data)
-    
-    characterDatas.comicsDetails = comicsDetailsDatas
+    const comicsDetails = await Promise.all(comicsDetailsPromises);
 
-    res.status(200).json(characterDatas)
+    const comicsDetailsDatas = comicsDetails.map((response) => response.data);
 
+    characterDatas.comicsDetails = comicsDetailsDatas;
 
-    } catch (error) {
-        res.status(error.status || 500).json(error.message || "Internal server error")
-    }
-    
-}
+    res.status(200).json(characterDatas);
+  } catch (error) {
+    res
+      .status(error.status || 500)
+      .json(error.message || "Internal server error");
+  }
+};
 
-
-module.exports = {getAllCharacters, getOneCharacter}
+module.exports = { getAllCharacters, getOneCharacter };
--- controllers/comicsCtrl.js
+++ controllers/comicsCtrl.js
@@ -1,30 +1,37 @@
-require("dotenv").config()
-const axios = require ("axios")
-
+require("dotenv").config();
+const axios = require("axios");
 
 const getAllComics = async (req, res) => {
-    
-    //prepare the query parameters for API request
-    const page = req.query.page;
-    const title = req.query.name || null;
+  //prepare the query parameters for API request
+  const page = req.query.page;
+  const title = req.query.name || null;
 
-    //pagination
-    const limit =100;
-    const skip = (page - 1) * 100
-    
-        try {
-            //fetch characters' datas
-            const fetchAllComics = await axios.get(`https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.API_KEY}${limit ? "&limit=" + limit : ""}${skip ? "&skip=" + skip : ""}${title ? "&title=" + title : ""}`)
-    
-            const comicsDatas = fetchAllComics.data
-    
-               res.status(200).json(comicsDatas)
-        } catch (error) {
-            res.status(error.status || 500).json(error.message || "Internal server error")
-        }
-    
-}
+  //pagination
+  const limit = 100;
+  const skip = (page - 1) * 100;
 
+  try {
+    //fetch characters' datas
+    const fetchAllComics = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${
+        process.env.API_KEY
+      }${limit ? "&limit=" + limit : ""}${skip ? "&skip=" + skip : ""}${
+        title ? "&title=" + title : ""
+      }`
+    );
 
+    const comicsDatas = fetchAllComics.data;
 
-module.exports = {getAllComics}
+    if (Object.keys(comicsDatas).includes("error")) {
+      throw { status: 500, message: "Internal server error, please refresh" };
+    }
+
+    res.status(200).json(comicsDatas);
+  } catch (error) {
+    res
+      .status(error.status || 500)
+      .json(error.message || "Internal server error");
+  }
+};
+
+module.exports = { getAllComics };
